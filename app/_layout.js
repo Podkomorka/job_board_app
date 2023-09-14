bc11d5cf4989b403d5a89974880e3a924ed5f136
Jonathan Podkomorka <jonathanpodko@gmail.com>
2023-09-13 23:57:24 -0400

    Finished header, added custom fonts, implemented welcome section

--- app/_layout.js
+++ app/_layout.js
@@ -1,7 +1,26 @@
 import { Stack } from 'expo-router'
+import { useCallback } from 'react'
+import { useFonts } from 'expo-font' // custom fonts
+import * as SplashScreen from 'expo-splash-screen'
+
+SplashScreen.preventAutoHideAsync() // when the app is initially loading this will make the splash screen visable
 
 const Layout = () => {
-  return <Stack />
+  const [fontsLoaded] = useFonts({
+    DMBold: require('../assets/fonts/DMSans-Bold.ttf'),
+    DMMedium: require('../assets/fonts/DMSans-Medium.ttf'),
+    DMRegular: require('../assets/fonts/DMSans-Regular.ttf'),
+  })
+
+  const onLayoutRootView = useCallback(async () => {
+    if(fontsLoaded) {
+      await SplashScreen.hideAsync() // we only want to show our homepage if the fonts have been loaded
+    }
+  }, [fontsLoaded])
+
+  if(!fontsLoaded) return null
+
+  return <Stack onLayout={onLayoutRootView}/>
 }
 
 export default Layout
